154cae0e557c6b8bdba8bb43910bd20e81f7bd57
Zakki Rizqurobbi <zakkirizqurb@gmail.com>
2022-06-09 18:32:42 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withCSS = require("@zeit/next-css")
+
+module.exports = withCSS({
+  cssLoaderOptions: {
+    url: false
+  }
+})
 const nextConfig = {
   reactStrictMode: true,
 }
-
 module.exports = nextConfig
