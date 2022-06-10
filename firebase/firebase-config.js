e6773c34ecba9e6698155f42d383a2248f312900
Zakki Rizqurobbi <zakkirizqurb@gmail.com>
2022-06-10 18:40:32 +0700

    PT-05 Fixing bug firebase

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,7 +1,6 @@
 import {initializeApp} from "firebase/app"
 import {getFirestore} from "firebase/firestore"
-
-const firebaseConfig = {
+const firebaseConfig = initializeApp({
     apiKey: process.env.NEXT_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -9,7 +8,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-};
+});
 
-export const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app);
+// export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(firebaseConfig);
